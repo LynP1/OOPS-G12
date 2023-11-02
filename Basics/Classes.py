13598b8d426d6b324410e758de4c4d69cd14c633
LynP1 <112970609+LynP1@users.noreply.github.com>
2023-11-02 15:08:44 +0000

    Updated

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -5,8 +5,14 @@ class Orange:
         self.colour = c
         self.weight = w
 
+    def rot(self, hours, temp):
+        days = hours / 24
+        self.mold =  round(days * temp,2)
+
 # Instantiating an Object
 
-orange_1 = Orange('Orange', 0.8)
-orange_2 = Orange('Red',0.6)
-orange_3 = Orange('Yellow-Org',0.4)
+navel_orange = Orange('Orange', 0.8)
+blood_orange_2 = Orange('Red',0.6)
+tangerine = Orange('Yellow-Org',0.4)
+tangerine.rot(22,21)
+print(tangerine.mold)
