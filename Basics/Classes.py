b5baea1005b0d8b6e7a1bae14e97c4a519f9053a
LynP1 <112970609+LynP1@users.noreply.github.com>
2023-11-02 14:51:48 +0000

    Updates

---
+++ Basics/Classes.py
@@ -0,0 +1,12 @@
+# Defining a Class
+
+class Orange:
+    def __init__(self, c, w):
+        self.colour = c
+        self.weight = w
+
+# Instantiating an Object
+
+orange_1 = Orange('Orange', 0.8)
+orange_2 = Orange('Red',0.6)
+orange_3 = Orange('Yellow-Org',0.4)
