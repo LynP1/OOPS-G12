260965ab76518a7ec65297134afa638b26b8315c
LynP1 <112970609+LynP1@users.noreply.github.com>
2023-11-08 10:24:36 -0500

    Update Bank1.py

--- Basics/Banking Example/Bank1.py
+++ Basics/Banking Example/Bank1.py
@@ -3,12 +3,9 @@ from BankAccount import *
 
 LigaBanka = []
 
-SonsAccount = Account('Sonny','HMS7',253850)
-LigaBanka.append(SonsAccount)
-PagesAccount = Account('Page','Pageword',72)
-LigaBanka.append(PagesAccount)
-SilvasAccount = Account('TSilva','Chelsea1',0)
-LigaBanka.append(SilvasAccount)
+SonsAccount = Account(LigaBanka,'Sonny','HMS7',253850)
+PagesAccount = Account(LigaBanka,'Page','Pageword',72)
+SilvasAccount = Account(LigaBanka,'TSilva','Chelsea1',0)
 
 PagesAccount.transfer(LigaBanka,'TSilva',72,'Pageword')
 SilvasAccount.view_balance('Chelsea1')
