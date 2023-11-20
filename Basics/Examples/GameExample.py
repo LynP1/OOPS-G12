6044bf637e042fbf424191b8fbf5b92db5ab4b61
LynP1 <112970609+LynP1@users.noreply.github.com>
2023-11-20 15:26:13 +0000

    Stuff

--- Basics/Examples/GameExample.py
+++ Basics/Examples/GameExample.py
@@ -1,6 +1,5 @@
 class Item():
-    def __init__(self):
-        pass
+    pass
 
 class Food(Item):
     pass
@@ -12,27 +11,13 @@ class Material(Item):
     pass
 
 class Being():
-    def __init__(self):
-        self.health = 100
-        self.damage = 5
+    pass
 
 class Player(Being):
-    def __init__(self,name):
-        self.name = name
-        self.exp = 0
-        self.level = 1
-        self.health = 100
-        self.damage = 5
-        self.inventory = []
+    pass
 
 class Enemy(Being):
-    def __init__(self):
-        self.level = 1
-        self.health = 100
-        self.damage = 5
-        self.drop_table = []
+    pass
 
 class Animal(Being):
-    def __init__(self):
-        self.health = 100
-        self.drop_table = []
+    pass
